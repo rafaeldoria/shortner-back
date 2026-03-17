9836875d6e4abed4f11e93796f47c164b65ccceb
rafaeldoria <ieieldoria@gmail.com>
2026-03-17 19:04:02 -0300

    fix: add cors.

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,18 @@ import authRoutes from "./modules/auth/auth.routes";
 import urlRoutes from "./modules/url/url.routes";
 import rateLimit from "express-rate-limit";
 
+var cors = require('cors');
+
 const app = express();
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://shortner.it-rod.com"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 const globalLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutos
