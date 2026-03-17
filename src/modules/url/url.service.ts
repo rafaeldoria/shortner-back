ee742374eeff580e7f70c45fa6d36b714ec5579d
rafaeldoria <ieieldoria@gmail.com>
2026-03-17 20:20:53 -0300

    fix: limit urls.

--- src/modules/url/url.service.ts
+++ src/modules/url/url.service.ts
@@ -4,6 +4,12 @@ import { UrlModel } from "./url.model";
 export class UrlService {
 
   async create(originalUrl: string, userId: string) {
+    const count = await UrlModel.countDocuments({ userId });
+
+    if (count >= 5) {
+      throw new Error("You have reached the maximum of 5 URLs");
+    }
+
     const code = nanoid(7);
 
     const url = await UrlModel.create({
